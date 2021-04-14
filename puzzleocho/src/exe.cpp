c5f89c525641c526f9d08cc2cf10c0998e937b81
Nicolas3094 <nicoo.garcia@gmail.com>
2021-04-14 12:11:21 -0600

    "cm2"

--- puzzleocho/src/exe.cpp
+++ puzzleocho/src/exe.cpp
@@ -1,12 +1,15 @@
 #include "../headers/exe.hpp"
 
 void exePila(){
-    Tablero* tab = new Tablero();
-    Arbol* arb = new Arbol(*tab);
-    Estado* estadoInicial = tab->EstadoInicial();
+    Tablero tab;
+    Arbol* arb = new Arbol(tab);
+    Estado* estadoInicial = tab.EstadoInicial();
     print("Estado Inicial: ");
-    tab->Mostrar(*estadoInicial);
+    tab.Mostrar(*estadoInicial);
     print("Iniciar con busqueda.");
     cin.get();
+    
     arb->BFS(*estadoInicial);
+
+    delete arb;
 }
